dd32f1cd7fcf0551d97d2fb254183f9612cc1700
Hoda Amirianfar <57070880+hoda-amirianfar@users.noreply.github.com>
2020-10-28 12:58:00 +0100

    create layouts.footer
    then includes in layouts.app

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,25 @@
+
+<div class="container">
+    <div class="row fixed-bottom">
+        <div class="col-md-12 border d-flex justify-content-between">
+                    <div class="ml-4 text-left text-sm text-gray-500 sm:text-right sm:ml-0">
+                        <a href="https://www.linkedin.com/in/hoda-amirianfar-577a3b89/" class="ml-1 underline">
+                                Created By Hoda Amirianfar
+                        </a>
+                    </div>
+
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Build on Oktobor 2020
+                    </div>
+
+                    <div class="ml-4 text-right text-sm text-gray-500 sm:text-right sm:ml-0">
+                        <a href="https://github.com/hoda-amirianfar/event-manager" class="ml-1 underline">
+                                Would you like to Clone it from Github!
+                        </a>
+                    </div>
+        </div>
+    </div>
+</div> 
+
+
+
