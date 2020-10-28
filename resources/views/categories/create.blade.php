d24e5cd35a1934e7546ca76d689d92f0ae97f278
Hoda Amirianfar <57070880+hoda-amirianfar@users.noreply.github.com>
2020-10-28 13:08:41 +0100

    update categories.create

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,18 +1,13 @@
 <tr>
+<form method="post" action="/categories" name="add-form" >
                         <td class="pt-3-half"></td>
                         <td class="pt-3-half">
-                            <form method="post" action="/categories" name="add-form" >
                                 @csrf
-                                <div class="form-group">
                                 <input type="text" class="form-control form-control-sm" maxlength="50" name="title" id="title" value="" placeholder="Title" class="" required />
-                                
-                                </div>
-                            </form>
                         </td>
                         <td class="text-center" colspan="2">
-                                <a href="/categories" class="text-success" onclick="event.preventDefault();
-                                        document.getElementById('add-form').submit();">
-                                    <i data-feather="plus" aria-hidden="true"></i>
-                                </a>
+                            <button type="submit" class="btn btn-success btn-rounded btn-sm my-0">Add</button> 
                         </td>
+                                                           
+                        </form>
                     </tr>
