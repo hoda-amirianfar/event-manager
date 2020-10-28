817b7717855032607cdd6c8f8b5dd8ef4e8cdcb2
Hoda Amirianfar <57070880+hoda-amirianfar@users.noreply.github.com>
2020-10-28 13:09:30 +0100

    update CategoryController method index with orderBy

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::orderBy('created_at', 'DESC')->get();
         return view('categories.index', compact('categories'));
     }
 
