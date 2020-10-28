f9cd101bd67357d70a3600a4c9af999e04893ce0
Hoda Amirianfar <57070880+hoda-amirianfar@users.noreply.github.com>
2020-10-28 14:40:02 +0100

    update categories.create temp with auth

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,13 +1,15 @@
+@auth
 <tr>
-<form method="post" action="/categories" name="add-form" >
-                        <td class="pt-3-half"></td>
-                        <td class="pt-3-half">
-                                @csrf
-                                <input type="text" class="form-control form-control-sm" maxlength="50" name="title" id="title" value="" placeholder="Title" class="" required />
-                        </td>
-                        <td class="text-center" colspan="2">
-                            <button type="submit" class="btn btn-success btn-rounded btn-sm my-0">Add</button> 
-                        </td>
+    <form method="post" action="/categories" name="add-form" >
+        <td class="pt-3-half"></td>
+        <td class="pt-3-half">
+            @csrf
+            <input type="text" class="form-control form-control-sm" maxlength="50" name="title" id="title" value="" placeholder="Title" class="" required />
+        </td>
+        <td class="text-center" colspan="2">
+            <button type="submit" class="btn btn-success btn-rounded btn-sm my-0">Add</button> 
+        </td>
                                                            
-                        </form>
-                    </tr>
+    </form>
+</tr>
+@endauth
