af8b3842130347a2c17118748119460b573b1172
Hoda Amirianfar <57070880+hoda-amirianfar@users.noreply.github.com>
2020-10-29 08:43:07 +0100

    EventController method show

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -47,9 +47,10 @@ class EventController extends Controller
      * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function show(Event $event)
+    public function show($id)
     {
-        //
+        $event = Event::findOrFail($id);
+        return view('events.show', compact('event'));
     }
 
     /**
